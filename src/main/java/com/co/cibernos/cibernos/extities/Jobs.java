3cb8d6634e34225989d980befcb34e0d31ab15e5
brian.zambrano <brian.zambrano@itac.co>
2021-04-25 18:11:51 -0500

    Crud jsf - mariadb

---
+++ src/main/java/com/co/cibernos/cibernos/extities/Jobs.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.co.cibernos.cibernos.extities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author brian
+ */
+@Entity
+@Table(name = "jobs")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Jobs.findAll", query = "SELECT j FROM Jobs j")
+    , @NamedQuery(name = "Jobs.findByJobId", query = "SELECT j FROM Jobs j WHERE j.jobId = :jobId")
+    , @NamedQuery(name = "Jobs.findByName", query = "SELECT j FROM Jobs j WHERE j.name = :name")
+    , @NamedQuery(name = "Jobs.findByDescription", query = "SELECT j FROM Jobs j WHERE j.description = :description")})
+public class Jobs implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "job_id")
+    private Integer jobId;
+    @Size(max = 100)
+    @Column(name = "Name")
+    private String name;
+    @Size(max = 300)
+    @Column(name = "description")
+    private String description;
+
+    public Jobs() {
+    }
+
+    public Jobs(Integer jobId) {
+        this.jobId = jobId;
+    }
+
+    public Integer getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Integer jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (jobId != null ? jobId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Jobs)) {
+            return false;
+        }
+        Jobs other = (Jobs) object;
+        if ((this.jobId == null && other.jobId != null) || (this.jobId != null && !this.jobId.equals(other.jobId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.co.cibernos.cibernos.extities.Jobs[ jobId=" + jobId + " ]";
+    }
+    
+}
---
+++ target/generated-sources/annotations/com/co/cibernos/cibernos/extities/Jobs_.java
@@ -0,0 +1,15 @@
+package com.co.cibernos.cibernos.extities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-25T18:05:29")
+@StaticMetamodel(Jobs.class)
+public class Jobs_ { 
+
+    public static volatile SingularAttribute<Jobs, Integer> jobId;
+    public static volatile SingularAttribute<Jobs, String> name;
+    public static volatile SingularAttribute<Jobs, String> description;
+
+}
